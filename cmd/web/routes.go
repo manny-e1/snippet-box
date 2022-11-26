4a8c6db3a29884638f8d668f8d070f0e0691cbd8
manny-e1 <aewnetu6@gmail.com>
2022-11-26 11:41:04 +0300

    panic recovery middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,5 +14,5 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return app.logRequest(secureHeaders(mux))
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
