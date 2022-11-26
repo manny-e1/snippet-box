0f83f7a1a42a181d9708614039f73fdaddd3c520
manny-e1 <aewnetu6@gmail.com>
2022-11-26 11:13:59 +0300

    security middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -14,5 +14,5 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return mux
+	return secureHeaders(mux)
 }
