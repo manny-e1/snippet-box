b2404a3625f99c71fbb02efaedd55fd9e633d16b
manny-e1 <aewnetu6@gmail.com>
2022-12-02 13:36:55 +0300

    validation helper methods and functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,9 +5,17 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/manny-e1/snippetbox/internal/models"
+	"github.com/manny-e1/snippetbox/internal/validator"
 	"net/http"
 	"strconv"
 )
+
+type snippetCreateForm struct {
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
+}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
@@ -46,10 +54,15 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) createSnippetPost(w http.ResponseWriter, r *http.Request) {
+	// Limit the request body size to 4096 bytes
+	//r.Body = http.MaxBytesReader(w, r.Body, 4096)
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
@@ -63,7 +76,27 @@ func (app *application) createSnippetPost(w http.ResponseWriter, r *http.Request
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	id, err := app.snippets.Insert(title, content, expires)
+
+	form := snippetCreateForm{
+		Title:   title,
+		Content: content,
+		Expires: expires,
+	}
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field can't be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field can't be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+
+	if !form.IsValid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		fmt.Println(data.Form)
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
+
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.errorLogger.Println(err.Error())
 		app.serverError(w, err)
