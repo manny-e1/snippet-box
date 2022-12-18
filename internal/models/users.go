db111d8dc92843260ef6ab85cdb9144386bdef58
manny-e1 <aewnetu6@gmail.com>
2022-12-18 14:46:44 +0300

    user insert method

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,6 +2,10 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
 	"time"
 )
 
@@ -18,6 +22,22 @@ type UserModel struct {
 }
 
 func (um *UserModel) Insert(name, email, password string) error {
+	hashedPwd, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	stmt := `INSERT INTO users (name,email,password,created) 
+			VALUES (?,?,?,UTC_TIMESTAMP())`
+	_, err = um.DB.Exec(stmt, name, email, hashedPwd)
+	if err != nil {
+		var mySQLErr *mysql.MySQLError
+		if errors.As(err, &mySQLErr) {
+			if mySQLErr.Number == 1862 && strings.Contains(mySQLErr.Message, "users_us_email") {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 func (um *UserModel) Authenticate(email, password string) (int, error) {
