6b9c3f80ed2d74b9eb9be176ae8983f30f1cf821
manny-e1 <aewnetu6@gmail.com>
2022-12-20 12:11:04 +0300

    Ping handler Test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/manny-e1/snippetbox/internal/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	rr := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	Ping(rr, r)
+	rs := rr.Result()
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, string(body), "OK")
+}
